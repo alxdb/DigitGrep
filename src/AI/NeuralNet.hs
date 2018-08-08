b900ab88befa47d8f992e14950512e4f7e0308a7
Alexander Davidson Bryan <alex.davidsonbryan@gmail.com>
2018-08-08 21:33:50 +0100

    Completed Refactor, also implemented backprop

--- src/AI/NeuralNet.hs
+++ src/AI/NeuralNet.hs
@@ -1,7 +1,8 @@
 module AI.NeuralNet (
 generateNeuralNet,
 saveNetwork,
-loadNetwork
+loadNetwork,
+train
 ) where
 
 import           Data.Binary
@@ -15,8 +16,11 @@ type NeuralNet = [Layer]
 -- a tuple of the weights connecting the previous layer to this one, and the biases of this layer
 type Layer = (Weights, Biases)
 -- |Gradient
--- a tuple containing the gradient of the weights, and biases in a layer
-type Nabla = (Matrix R, Vector R)
+-- a tuple containing the gradient of the weights, and biases in a network
+type Gradient = [Nabla]
+-- |Nabla
+-- the gradient for an individual layer
+type Nabla = (Weights, Biases)
 
 type Weights = Matrix R
 type Biases = Vector R
@@ -25,11 +29,11 @@ type Biases = Vector R
 generateNeuralNet :: Int -> [Int] -> NeuralNet
 generateNeuralNet seed sizes = map genRandLayer dims
     where
-        g = mkStdGen seed
-        dims = zip sizes (tail sizes)
+        genRandLayer (input, output) = (genRandWeights output input, genRandBiases output)
         genRandWeights rows cols = rows><cols $ randoms g
         genRandBiases size = size |> randoms g
-        genRandLayer (input, output) = (genRandWeights output input, genRandBiases output)
+        g = mkStdGen seed
+        dims = zip sizes (tail sizes)
 
 
 saveNetwork :: FilePath -> NeuralNet -> IO ()
@@ -53,7 +57,7 @@ feedFore (a, z) layer = (a' : a, z' : z)
 
 -- |Feedback algorithm for BackPropagation
 -- takes a list of nablas, the current zs, the backwards activations and the forward weights
-feedBack :: [Nabla] -> (Vector R, Vector R, Weights) -> [Nabla]
+feedBack :: Gradient -> (Vector R, Vector R, Weights) -> Gradient
 feedBack nabla (z, a, w)  = nabla' : nabla
     where
         nabla' = (nabla_w, nabla_b)
@@ -62,13 +66,24 @@ feedBack nabla (z, a, w)  = nabla' : nabla
 
 -- |Backpropagation algorithm
 -- performs backpropagation on a network, and a training pair using feedFore and feedBack
-backProp :: NeuralNet -> (Vector R, Vector R) -> [Nabla]
+backProp :: NeuralNet -> (Vector R, Vector R) -> Gradient
 backProp network (input, output) = foldl feedBack [(asColumn initDel * asRow initAct, initDel)] backSet
     where
-        foreRes = foldl feedFore ([input], []) network
         initDel = ((head . fst $ foreRes) - output) * cmap sigmoid' (head . snd $ foreRes)
         initAct = fst foreRes !! 1
+        foreRes = foldl feedFore ([input], []) network
         backSet = map (\i -> (snd foreRes !! i, fst foreRes !! (i + 1), fst (reverse network !! (i - 1)))) [1..length network - 1]
+
+-- |Training Function
+-- applys back propagation to a network given a set of input data
+train :: NeuralNet -> R -> [(Vector R, Vector R)] -> NeuralNet
+train network eta ioPairs = mapPair applyDel (network, grad)
+    where
+        applyDel :: Layer -> Nabla -> Layer
+        applyDel (w, b) (dw, db) = (w - scalar (eta / fromIntegral (length ioPairs)) * dw, b - scalar (eta / fromIntegral (length ioPairs)) * db)
+        grad = foldl (zipWith addPair) grad_init del_grads
+        grad_init = map (\l -> (konst 0 . size . fst $ l, konst 0 . size . snd $ l)) network
+        del_grads = map (backProp network) ioPairs
 
 -- Util
 
@@ -77,3 +92,10 @@ sigmoid z = 1.0 / (1.0 + exp (-z))
 
 sigmoid' :: R -> R
 sigmoid' z = sigmoid z * (1 - sigmoid z)
+
+mapPair :: (a -> b -> c) -> ([a], [b]) -> [c]
+mapPair _ ([], [])     = []
+mapPair f (x:xs, y:ys) = f x y : mapPair f (xs, ys)
+
+addPair :: (Num a, Num b) => (a, b) -> (a, b) -> (a, b)
+addPair (xa, xb) (ya, yb) = (xa + ya, xb + yb)
---
+++ src/Data/Mnist.hs
@@ -0,0 +1,26 @@
+module Data.Mnist (getTrainingData, getTestingData) where
+
+import           Data.IDX
+import           Data.Vector.Unboxed   as VB
+import           Numeric.LinearAlgebra as HM
+
+getTrainingData :: IO [(HM.Vector R, HM.Vector R)]
+getTrainingData = getMnistData "data/train-images.idx" "data/train-labels.idx"
+
+getTestingData :: IO [(HM.Vector R, HM.Vector R)]
+getTestingData = getMnistData "data/test-images.idx" "data/test-labels.idx"
+
+getMnistData :: FilePath -> FilePath -> IO [(HM.Vector R, HM.Vector R)]
+getMnistData images labels= do
+    imageData <- decodeIDXFile images
+    labelData <- decodeIDXLabelsFile labels
+    return [ (vector $ VB.toList image, vector [if x == label then 1.0 else 0.0 | x <- [0..9]]) | (label, image) <- fromJust ((labeledDoubleData >? labelData) >? imageData)]
+
+-- Utils
+
+fromJust :: Maybe a -> a
+fromJust Nothing  = error "found Nothing"
+fromJust (Just x) = x
+
+(>?) :: (a -> b) -> Maybe a -> b
+(>?) f x = f (fromJust x)
